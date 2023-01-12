3e4c32f77f9b003acff204b59cc1b56e9b1e8e47
Vincent Washington <kuraiokamiblaze@gmail.com>
2023-01-12 04:05:24 +0000

    Update logic for getting pokemon names and ids

--- js/main.js
+++ js/main.js
@@ -1,42 +1,54 @@
 const root = document.documentElement;
-/* Class Variables */
-const hideClass = "hide";
-const openClass = "open";
-/* Element Variables */
+/* Menu Operator */
 const sideMenu = document.querySelector("#sideMenu");
 const sideMenuSwitches = document.querySelectorAll(".menu-btn");
 const headMenuSwitch = document.querySelector(".menu-btn.head");
-const menuLinks = document.querySelectorAll(".menu-link");
-const homeView = document.querySelector(".home-box");
 // handle hide class
 function toggleHideClass(ele) {
+  const hideClass = "hide";
+
   !ele.className.includes(hideClass)
     ? ele.classList.add(hideClass)
     : ele.classList.remove(hideClass);
 }
 // Handle open class
 function toggleOpenClass(ele) {
+  const openClass = "open";
+
   !ele.className.includes(openClass)
     ? ele.classList.add(openClass)
     : ele.classList.remove(openClass);
 }
-
 // Handle menu open/close
 sideMenuSwitches.forEach((btn) => {
   btn.addEventListener("click", () => {
     toggleOpenClass(sideMenu);
   });
 });
-// link behavior
-menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    if (!homeView.className.includes(hideClass)) {
-      toggleHideClass(homeView);
-      toggleHideClass(headMenuSwitch);
-    }
-    toggleOpenClass(sideMenu);
-  });
-});
+
+/* API Calls */
+async function getAllPokemon() {
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=1008&offset=0";
+
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+    return data.results;
+  } catch (err) {
+    console.log(err);
+  }
+}
+// ID Handler
+function generateDexId(id) {
+  let dexId = "";
+
+  if (id < 10) dexId = "00" + id;
+  else if (id >= 10 && id < 100) dexId = "0" + id;
+  else dexId = id.toString();
+
+  return dexId;
+}
+
 /* Handle marquee animation */
 const marqueeContent = document.querySelector("ul.marquee-content");
 const marqueeLength = marqueeContent.children.length;
